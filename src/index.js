be6fab95ecee49976212f56281a1e2fc24e66cea
Alina Fedorova <sapereaude.alf@gmail.com>
2024-08-20 22:11:29 +0500

    feat: re2

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ module.exports = function towelSort (matrix) {
 
     if (!matrix || matrix.length === 0) return newArr;
       
-    for (let i = matrix.length; i < matrix.length; i--) {
+    for (let i = 0; i < matrix.length; i++) {
   
       if (i % 2 === 0) {
         newArr = newArr.concat(matrix[i]);
